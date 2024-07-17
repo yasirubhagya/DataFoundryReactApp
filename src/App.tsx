f32ba1ec20aadbe06d273267119b543e455baad7
yasirubhagya <yasirubhagya@gmail.com>
2024-07-17 14:39:21 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { ServiceRequestForm } from "./components/ServiceRequestForm";
+import { ServiceRequestList } from "./components/ServiceRequestList";
+import "@aws-amplify/ui-react/styles.css"
 
-const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div className="flex w-full h-dvh justify-center items-center">
+      <Authenticator>
+        {({ signOut }) => (
+          <main className="flex flex-col w-full h-full">
+            <div className="flex w-full h-24 border border-gray-900/10 p-4 shadow fixed top-0 bg-white">
+              <button onClick={signOut}>Sign out</button>
+            </div>
+            <div className="flex flex-col lg:flex-row w-full h-full pt-24">
+              <div className="flex justify-center items-top">
+                <ServiceRequestForm />
+              </div>
+              <div className="flex justify-center">
+                <ServiceRequestList />
+              </div>
+
+            </div>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
 }
 
